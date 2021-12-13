2b175229b3779d49bbea1417d7306e4bc23ae536
Chris Delong <admin@frosthaven.net>
2021-12-13 15:15:10 -0700

    fixed user issue

--- JPAPhotoNerds/src/main/java/com/skilldistillery/photonerds/entities/User.java
+++ JPAPhotoNerds/src/main/java/com/skilldistillery/photonerds/entities/User.java
@@ -1,12 +1,13 @@
 package com.skilldistillery.photonerds.entities;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 public class User {
@@ -14,8 +15,23 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+	@Column(name = "username")
 	private String username;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "enabled")
+	private int enabled;
+	@Column(name = "role")
+	private String role;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "join_date")
+	private LocalDateTime joinDate;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	
 
 	public int getId() {
 		return id;
@@ -32,10 +48,68 @@ public class User {
 	public void setUsername(String userName) {
 		this.username = userName;
 	}
+	
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(int enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDateTime getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(LocalDateTime joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		return Objects.hash(email, enabled, firstName, id, joinDate, lastName, password, role, username);
 	}
 
 	@Override
@@ -47,12 +121,18 @@ public class User {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return id == other.id;
+		return Objects.equals(email, other.email) && enabled == other.enabled
+				&& Objects.equals(firstName, other.firstName) && id == other.id
+				&& Objects.equals(joinDate, other.joinDate) && Objects.equals(lastName, other.lastName)
+				&& Objects.equals(password, other.password) && Objects.equals(role, other.role)
+				&& Objects.equals(username, other.username);
 	}
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", userName=" + username + "]";
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", enabled=" + enabled
+				+ ", role=" + role + ", email=" + email + ", joinDate=" + joinDate + ", firstName=" + firstName
+				+ ", lastName=" + lastName + "]";
 	}
 	
 	
