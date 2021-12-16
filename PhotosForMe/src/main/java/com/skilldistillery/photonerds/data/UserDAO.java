eb54c8efab58d8da52ba6b3594733cefc12958c2
Andrew Shumway <shumway.andrew@yahoo.com>
2021-12-16 10:05:50 -0700

    Implement methods in Impl to test to website

--- PhotosForMe/src/main/java/com/skilldistillery/photonerds/data/UserDAO.java
+++ PhotosForMe/src/main/java/com/skilldistillery/photonerds/data/UserDAO.java
@@ -1,9 +1,14 @@
 package com.skilldistillery.photonerds.data;
 
+import java.util.List;
+
+import com.skilldistillery.photonerds.entities.Photographer;
 import com.skilldistillery.photonerds.entities.User;
 
 public interface UserDAO {
 
 	User findByUsername(String username);
+	User updateUserInfo(int id);
+	List<Photographer> findPhotographers(int id); 
 	
 }
