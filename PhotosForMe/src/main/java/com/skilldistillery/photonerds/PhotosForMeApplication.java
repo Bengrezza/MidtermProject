dfa22477e784691d6f195b0dd2074a143d9a99f8
Bengrezza <bengretza@gmail.com>
2021-12-17 15:03:19 -0700

    Added AWS Functionality

--- PhotosForMe/src/main/java/com/skilldistillery/photonerds/PhotosForMeApplication.java
+++ PhotosForMe/src/main/java/com/skilldistillery/photonerds/PhotosForMeApplication.java
@@ -2,12 +2,16 @@ package com.skilldistillery.photonerds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PhotosForMeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PhotosForMeApplication.class, args);
-	}
-
-}
+public class PhotosForMeApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(PhotosForMeApplication.class);
+	  }
+	  public static void main(String[] args) {
+		    SpringApplication.run(PhotosForMeApplication.class, args);
+		  }
+		}
